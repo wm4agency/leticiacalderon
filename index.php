2c49f1959617307a34c570964fe6c5066d210020
mono-entrenado <oscar@wm4.mx>
2018-06-07 18:43:02 -0500

    texto "descarga" y cambios a css previos

--- index.php
+++ index.php
@@ -27,8 +27,7 @@ if (! include_once "p/partials/head.php"){echo "no head found";die();}
       <?php include_once "p/partials/s_decalogo.php"; ?>
     </section>
     <section id="galeria">
-      <div id="galeria-DOM" class="galeria galeria-DOM"></div>
-      <div id="pswpGal" class="galeria"></div>
+      <?php include_once "p/partials/s_galeria.php"; ?>
     </section>
     <section id="audios">
       <div class="container"><?php include_once "p/components/audio_player.php"; ?></div>
---
+++ p/partials/s_galeria.php
@@ -0,0 +1,5 @@
+<div id="galeria-DOM" class="galeria galeria-DOM"></div>
+<header class="title">
+  <h1>¡Descarga tu foto con Leti!</h1>
+</header>
+<div id="pswpGal" class="galeria"></div>
